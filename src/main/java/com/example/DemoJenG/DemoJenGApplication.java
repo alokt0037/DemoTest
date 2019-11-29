91179ec77722736731dfe4bd9ae3c6e75f38f5fa
alokt0037 <45688878+alokt0037@users.noreply.github.com>
2019-11-29 15:26:02 +0530

    space added

--- src/main/java/com/example/DemoJenG/DemoJenGApplication.java
+++ src/main/java/com/example/DemoJenG/DemoJenGApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoJenGApplication {
+	
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenGApplication.class, args);
