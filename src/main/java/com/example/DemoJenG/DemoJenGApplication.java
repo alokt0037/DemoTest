884f858c49fb9dafbc2de2bb1189230f11f5a40a
alokt0037 <45688878+alokt0037@users.noreply.github.com>
2019-11-29 14:30:36 +0530

    sop added

--- src/main/java/com/example/DemoJenG/DemoJenGApplication.java
+++ src/main/java/com/example/DemoJenG/DemoJenGApplication.java
@@ -8,6 +8,7 @@ public class DemoJenGApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJenGApplication.class, args);
+		System.out.println("Hello spring boot Application");
 	}
 
 }
